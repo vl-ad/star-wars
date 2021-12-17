8b0a1a52ef6e218252d6624ef4d0b39853a5dbf6
vkulakov <kulakov.vladyslav@wdc.com>
2021-12-17 14:56:07 +0200

    Removing withRouter

--- src/Card/Star/index.jsx
+++ src/Card/Star/index.jsx
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
-import { withRouter } from 'react-router-dom';
 
 import FavouritesContext from '@contexts/favourites';
 
@@ -44,4 +43,4 @@ CardStar.propTypes = {
   }).isRequired,
 };
 
-export default withRouter(CardStar);
+export default CardStar;
